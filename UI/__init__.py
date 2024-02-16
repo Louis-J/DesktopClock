ced897dcd2cbbc7a641200641417fd2745e52934
Louis-J <czzcy3832515@gmail.com>
2024-02-16 15:13:57 +0800

    add clear logs

--- UI/__init__.py
+++ UI/__init__.py
@@ -1,20 +1,49 @@
 import sys
 import time
+import logging
+import os
 from .Frame import Frame
 from PySide6.QtWidgets import QApplication
-import logging
 
-logger = logging.getLogger(__name__)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh = logging.FileHandler(f'logs/{int(time.time())}.log')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-logger.addHandler(fh)
 
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+log_dir = 'logs'
+keep_time = 30*24*3600
+keep_num = 20
+
+def set_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh = logging.FileHandler(f'logs/{int(time.time())}.log')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    logger.info('logger start!')
+    return logger
+
+def clear_logs():
+    cur_time = time.time()
+    file_list = []
+    for file in os.listdir(log_dir):
+        file_path = os.path.join(log_dir, file)
+        modified_time = os.path.getmtime(file_path)
+        if cur_time - modified_time > keep_time:
+            os.remove(file_path)
+            logger.info(f'clear log file: {file_path}')
+        else:
+            file_list.append((modified_time, file_path))
+    if len(file_list) > keep_num:
+        file_list.sort(key = lambda x: -x[0])
+        for file_path in (i[1] for i in file_list[keep_num:]):
+            os.remove(file_path)
+            logger.info(f'clear log file: {file_path}')
+
+logger = set_logger()
+clear_logs()
 
 class UI:
     def __init__(self):
